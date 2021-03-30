b97a81c31997b7579e6d38346ecdbe65b284a3b0
aysegulbetul <81184882+aysegulbetul@users.noreply.github.com>
2021-03-30 20:49:32 +0200

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		System.out.println("   ^   ");
+		System.out.println("  / \\ ");
+		System.out.println(" /   \\");
+		System.out.println("/     \\");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("-------");
+		
+		
+		
+		
+	}
+
+}
---
+++ Question8.java
@@ -0,0 +1,36 @@
+package day2;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("******             *************         **********");
+		System.out.println("**    **                  **             ** ");
+		System.out.println("**      **                **             ** ");
+		System.out.println("**        **              **             ** ");
+		System.out.println("**        **              **             ********");
+		System.out.println("**        **       **     **             **");
+		System.out.println("**      **          **    **             ** ");
+		System.out.println("**    **             **   **             ** ");
+		System.out.println("*****                  ****              ***********");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
